6b8750a280e665ae64193905da50e8184e1c9ffb
delta <delta@deltalab.dev>
2025-10-24 14:38:16 +0900

    feat: implement lock

--- rv/value.go
+++ rv/value.go
@@ -4,33 +4,57 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"runtime"
 	"time"
 
 	"github.com/fxamacker/cbor/v2"
 	"github.com/redis/rueidis"
+	"github.com/redis/rueidis/rueidislock"
 )
 
 type Value[T any] struct {
 	client rueidis.Client
+	locker rueidislock.Locker
 	key    string
 
 	config valueConfig
 }
 
 type valueConfig struct {
-	expires *time.Duration
+	expires         *time.Duration
+	lockKeyMajority int32
 }
 
 type Option func(*valueConfig)
 
-func NewValue[T any](client rueidis.Client, key string, options ...Option) *Value[T] {
-	r := &Value[T]{client: client, key: key}
+func NewValue[T any](clientOption rueidis.ClientOption, key string, options ...Option) *Value[T] {
+	client, err := rueidis.NewClient(clientOption)
+	if err != nil {
+		panic(fmt.Errorf("failed to create redis client: %w", err))
+	}
+
+	r := &Value[T]{key: key, client: client}
 
 	if len(options) > 0 {
 		for _, opt := range options {
 			opt(&r.config)
 		}
 	}
+
+	locker, err := rueidislock.NewLocker(rueidislock.LockerOption{
+		ClientOption:   clientOption,
+		NoLoopTracking: true,
+
+		KeyMajority: r.config.lockKeyMajority,
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed to create redis locker: %w", err))
+	}
+
+	r.locker = locker
+
+	runtime.AddCleanup(r, func(locker rueidislock.Locker) { locker.Close() }, r.locker)
+	runtime.AddCleanup(r, func(client rueidis.Client) { client.Close() }, r.client)
 
 	return r
 }
@@ -39,6 +63,24 @@ func WithDefaultExpiration(duration time.Duration) Option {
 	return func(r *valueConfig) {
 		r.expires = &duration
 	}
+}
+
+func WithKeyMajority(n int32) Option {
+	return func(r *valueConfig) {
+		r.lockKeyMajority = n
+	}
+}
+
+// WithLock acquires a distributed lock for the given key and executes the provided function within the lock's context.
+// It does not mean that the key itself is locked, but rather a namespaced lock based on the provided key.
+func (r *Value[T]) WithLock(ctx context.Context, key string, fn func(ctx context.Context) error) error {
+	ctx, release, err := r.locker.WithContext(ctx, r.key+":"+key)
+	if err != nil {
+		return fmt.Errorf("failed to acquire lock: %w", err)
+	}
+	defer release()
+
+	return fn(ctx)
 }
 
 func (r *Value[T]) Set(ctx context.Context, key string, value *T) error {
